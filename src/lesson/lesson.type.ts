b4caaf71aa3c58ec9839981f48e54399fd92a9a1
jiimmymunoz <myappeu@gmail.com>
2021-07-13 09:09:27 +0200

    feat: add graphqla module

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,16 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class LessonType {
+  @Field((type) => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+}
